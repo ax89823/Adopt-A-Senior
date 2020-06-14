1e03a094e43483b340b39a3ec04d01e1b030e5e4
gabytran <65130185+gabytran@users.noreply.github.com>
2020-06-14 11:27:06 -0700

    change javascript

--- script.js
+++ script.js
@@ -1,15 +1,35 @@
 /**
  * 
  */
-function showDeliveries(){
+var navhtml = '<ul>\
+	<li><a class="home" href="home.html">Home</a></li>\
+	<li><a class="signup" href="signup.html">Sign Up</a></li>\
+	<li><a class= "resources" href="resources.html">Resources</a></li>\
+	<li><a class="news" href="news.html">Good News</a></li>\
+	<li><a class="feedback" href="feedback.html">Feedback</a></li>\
+	<li><a class="login" href="login.html">Login</a></li>\
+	</ul>'
+	;
+var nav = document.getElementsByClassName("main");
+nav[0].innerHTML = navhtml;
+
+function showDeliveries() {
 	var deliveries = document.getElementById("deliveries");
-deliveries.style.display = 'grid';
-deliveries.style.gridTemplateColumns ='repeat(2, 1fr)';
-document.getElementById("contact").style.display = 'none';
+	deliveries.style.display = 'grid';
+	deliveries.style.gridTemplateColumns = 'repeat(2, 1fr)';
+	document.getElementById("contact").style.display = 'none';
 }
 
-function showContact(){
+function showContact() {
 	document.getElementById("contact").style.display = 'block';
 	document.getElementById("deliveries").style.display = 'none';
 }
+
+function addItem() {
+	
+}
+
+
+
+
 

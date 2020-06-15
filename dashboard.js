6a70893bdeba5143eeccbfde0823e4177f1ed271
gabytran <65130185+gabytran@users.noreply.github.com>
2020-06-14 17:38:36 -0700

    Dashboard formatting

--- dashboard.js
+++ dashboard.js
@@ -1,18 +1,3 @@
-/**
- * 
- */
-
-var navhtml = '<ul>\
-	<li><a class="home" href="home.html">Home</a></li>\
-	<li><a class="signup" href="signup.html">Sign Up</a></li>\
-	<li><a class= "resources" href="resources.html">Resources</a></li>\
-	<li><a class="news" href="news.html">Good News</a></li>\
-	<li><a class="feedback" href="feedback.html">Feedback</a></li>\
-	<li><a class="mydashboard" href="dashboard.html">My Dashboard</a></li>\
-	<li><a class="logout" href="home.html" >Logout</a></li></ul>';
-
-var nav = document.getElementsByClassName("main");
-nav[0].innerHTML = navhtml;
 
 function showDeliveries() {
 	var deliveries = document.getElementById("deliveries");
@@ -38,29 +23,58 @@ function closeModal() {
 }
 
 function addMedItem() {
+	/*Create containers*/
 	var med = document.createElement("LI");
 	var container = document.createElement("DIV")
+	var freqContainer = document.createElement("DIV");
+	var refillContainer = document.createElement("DIV");
+	
+	/*Get form input*/
+	var inputList = document.querySelectorAll(".modal-content input");
+	
+	/*Create header*/
 	var prescription = document.createElement("H4");
+	prescription.className = "name";
+	prescription.innerHTML =  inputList[0].value;
+	
+	/*Create Frequency elements*/
 	var frequencyLabel = document.createElement("H4");
 	frequencyLabel.innerHTML = "Frequency:";
 	var frequency = document.createElement("P");
+	frequency.innerHTML = inputList[1].value + " per week";
+	
+	/*Add to frequency div*/
+	freqContainer.appendChild(frequencyLabel);
+	freqContainer.appendChild(frequency);
+	
+	/* Create refill elements*/
 	var refillLabel = document.createElement("H4");
 	refillLabel.innerHTML = "Next refill:";
 	var refillDate = document.createElement("P");
-	var inputList = document.querySelectorAll(".modal-content input");
-	prescription.innerHTML =  inputList[0].value;
-	frequency.innerHTML = inputList[1].value + " per week";
+	
+	/*Create date*/
 	var date = new Date();
 	date.setTime(date.getTime() + (inputList[2].value/inputList[1].value * 86400000 * 7));
-	refillDate.innerHTML = date.toDateString();
-	container.appendChild(prescription);
-	container.appendChild(frequencyLabel);
-	container.appendChild(frequency);
-	container.appendChild(refillLabel);
-	container.appendChild(refillDate);
+	const options = {  year: 'numeric', month: 'long', day: 'numeric' };
+	refillDate.innerHTML = date.toLocaleDateString(undefined, options);
 	
+	
+	/*Add to refill div*/
+	refillContainer.appendChild(refillLabel);
+	refillContainer.appendChild(refillDate);
+	
+	/*Add to main div*/
+	container.appendChild(prescription);
+	container.appendChild(freqContainer);
+	container.appendChild(refillContainer);
+	
+	/*Add to list item*/
 	med.appendChild(container);
+	
+	/*Add to body*/
 	document.getElementById("med-list").appendChild(med);
+	
+	/*Close pop-up*/
 	closeModal();
 }
 function addGroceryItem() {
@@ -75,3 +89,15 @@ function openGroceryModal() {
 	document.getElementById("addGroceries").style.display = "block";
 	document.getElementById("overlay").style.display = "block";
 }
+
+var nav = document.getElementsByClassName("main");
+nav[0].innerHTML = '<ul>\
+	<li><a class="home" href="home.html">Home</a></li>\
+	<li><a class="signup" href="signup.html">Sign Up</a></li>\
+	<li><a class= "resources" href="resources.html">Resources</a></li>\
+	<li><a class="news" href="news.html">Good News</a></li>\
+	<li><a class="feedback" href="feedback.html">Feedback</a></li>\
+	<li><a class="mydashboard" href="dashboard.html">My Dashboard</a></li>\
+	<li><a class="logout" href="home.html" >Logout</a></li></ul>';
+
+

f2c77a0e082731d8c3b1a79faa6f247abf61a2a7
architen100 <architen100@gmail.com>
2020-06-14 19:19:07 -0700

    Add files via upload

---
+++ chatroom.js
@@ -0,0 +1,263 @@
+var is_start = true;
+var counter = 0;
+
+function getText() {
+        var chat_box = document.getElementById("chat_box");
+        var messages = document.getElementById("messages");
+        var robot = document.createElement("li");
+        var newMessage = document.createElement("li");
+
+        console.log(is_start);
+        console.log(counter)
+        if (is_start && counter == 0) {
+                var str = "Hello! Welcome to our symptom checker.Please answer yes or no to our questions and hit the SEND button.Are you expereincing trouble breathing, persistent pain or pressure in the chest, new confusion, inability to wake or saty awake, Bluish lips or face?";
+                robot.innerHTML = str;
+                messages.appendChild(robot);
+                chat_box.value = ""
+                // messages.appendChild(newMessage)
+                is_start = false;
+                counter += 1;
+                console.log(is_start);
+                console.log(counter)
+        }
+
+        var val = chat_box.value;
+        newMessage.innerHTML = val;
+        messages.appendChild(newMessage);
+        chat_box.value = ""
+        if (val == "yes" && counter == 1) {
+                var r1 = document.createElement("li");
+                var str = "Please call 911 Immediately. These are warning signs for COVID-19.";
+                r1.innerHTML = str;
+                messages.appendChild(r1);
+                chat_box.value = "";
+                // counter += 1;
+                return;
+        }
+        newMessage.innerHTML = val;
+        messages.appendChild(newMessage)
+        chat_box.value = ""
+        if (val == "no" && counter == 1) {
+                var str = "Are you experiencing fever or chills?";
+                robot.innerHTML = str;
+                messages.appendChild(robot);
+                chat_box.value = ""
+                // newMessage.innerHTML = chat_box.value;
+                // messages.appendChild(newMessage);
+                if (val == "yes" && counter == 1) {
+                        totalpoint += 1;
+                        counter += 1;
+                        return;
+                }
+                if (val = "no" && counter == 1) {
+                        totalpoint += 0;
+                        counter += 1;
+                        return;
+                }
+                return;
+        }
+        newMessage.innerHTML = val;
+        messages.appendChild(newMessage)
+        chat_box.value = ""
+        if (counter == 2) {
+                var str = "Are you experiencing a cough?"
+                robot.innerHTML = str;
+                messages.appendChild(robot);
+                chat_box.value = "";
+                // newMessage.innerHTML = chat_box.value
+                // messages.appendChild(newMessage)
+                if (val == "yes") {
+                        totalpoint+=1
+                        counter += 1;
+                }
+                if (val = "no"){
+                        var totalpoint = totalpoint + 0;
+                        counter += 1;
+                }
+                return;
+        }
+        newMessage.innerHTML = val;
+        messages.appendChild(newMessage)
+        chat_box.value = ""
+        if (counter == 3) {
+                var str = "Are you experiencing shortness of breath or having difficuly breathing?"
+                robot.innerHTML = str;
+                messages.appendChild(robot);
+                chat_box.value = "";
+                // newMessage.innerHTML = chat_box.value
+                // messages.appendChild(newMessage)
+                if (val == "yes") {
+                        totalpoint+=1
+                        counter +=1;
+                        return;
+                }
+                if (val == "no") {
+                        var totalpoint = totalpoint + 0;
+                        counter +=1;
+                        return;
+                }
+                return;
+        }
+        newMessage.innerHTML = val;
+        messages.appendChild(newMessage)
+        chat_box.value = ""
+        if (counter == 4){
+                var str = "Are you experiencing fatigue?"
+                robot.innerHTML = str;
+                messages.appendChild(robot);
+                chat_box.value = "";
+                // newMessage.innerHTML = chat_box.value
+                // messages.appendChild(newMessage)
+                if (val == "yes") {
+                        totalpoint+=1
+                        counter+=1
+                        return;
+                }
+                if (val = "no") {
+                        counter+=1
+                        return;
+                }
+                return;
+        }
+        newMessage.innerHTML = val;
+        messages.appendChild(newMessage)
+        chat_box.value = ""
+        if (counter == 5){
+                var str = "Are you experiencing muscle or body aches?"
+                robot.innerHTML = str;
+                messages.appendChild(robot);
+                chat_box.value = "";
+                // newMessage.innerHTML = chat_box.value
+                // messages.appendChild(newMessage)
+                if (val == "yes") {
+                        totalpoint+=1
+                        counter+=1;
+                        return;
+                }
+                if (val = "no") {
+                        var totalpoint = totalpoint + 0;
+                        counter+=1;
+                        return;
+                }
+                return;
+        }
+        newMessage.innerHTML = val;
+        messages.appendChild(newMessage)
+        chat_box.value = ""
+        if (counter == 6) {
+                var str = "Are you experiencing new loss of taste or smell?"
+                robot.innerHTML = str;
+                messages.appendChild(robot);
+                chat_box.value = "";
+                // newMessage.innerHTML = chat_box.value
+                // messages.appendChild(newMessage)
+                if (val == "yes") {
+                        totalpoint+=1
+                        counter+=1;
+                        return;
+                }
+                if (val = "no") {
+                        var totalpoint = totalpoint + 0;
+                        counter+=1;
+                        return;
+                }
+                return;
+        }
+        newMessage.innerHTML = val;
+        messages.appendChild(newMessage)
+        chat_box.value = ""
+        if (counter == 7){
+                var str = "Are you experiencing a sore throat?"
+                robot.innerHTML = str;
+                messages.appendChild(robot);
+                chat_box.value = "";
+                // newMessage.innerHTML = chat_box.value
+                // messages.appendChild(newMessage)
+                if (val == "yes") {
+                        totalpoint+=1
+                        counter+=1;
+                        return;
+                }
+                if (val = "no") {
+                        var totalpoint = totalpoint + 0;
+                        counter+=1;
+                        return;
+                }
+                return;
+        }
+        newMessage.innerHTML = val;
+        messages.appendChild(newMessage)
+        chat_box.value = ""
+        if (counter == 8){
+                var str = "Are you experiencing congestion or a runny nose?"
+                robot.innerHTML = str;
+                messages.appendChild(robot);
+                chat_box.value = "";
+                // newMessage.innerHTML = chat_box.value
+                // messages.appendChild(newMessage)
+                if (val == "yes") {
+                        totalpoint+=1
+                        counter+=1;
+                        return;
+                }
+                if (val = "no") {
+                        var totalpoint = totalpoint + 0;
+                        counter+=1;
+                        return;
+                }
+                return;
+        }
+        newMessage.innerHTML = val;
+        messages.appendChild(newMessage)
+        chat_box.value = ""
+        if (counter == 9) {
+                var str = "Are you experiencing Nausea?"
+                robot.innerHTML = str;
+                messages.appendChild(robot);
+                chat_box.value = "";
+                // newMessage.innerHTML = chat_box.value
+                // messages.appendChild(newMessage)
+                if (val == "yes") {
+                        totalpoint+=1
+                        counter+=1;
+                        return;
+                }
+                if (val = "no") {
+                        var totalpoint = totalpoint + 0;
+                        counter+=1;
+                        return;
+                }
+                return;
+        }
+        newMessage.innerHTML = val;
+        messages.appendChild(newMessage)
+        chat_box.value = ""
+        if (counter == 10){
+                var str = "Are you experiencing Diarrhea?"
+                robot.innerHTML = str;
+                messages.appendChild(robot);
+                chat_box.value = "";
+                // newMessage.innerHTML = chat_box.value
+                // messages.appendChild(newMessage)
+                if (val == "yes") {
+                        totalpoint += 1;
+                        counter +=1;
+                }
+                if (val = "no") {
+                        var totalpoint = totalpoint + 0;
+                        totalpoint += 1
+                        counter +=1;
+                }
+        }
+                if (totalpoint < 6 && counter == 11) {
+                        console.log("in")
+                        alert(" Symptoms show that you may be experiencing a high fever but it may not be COVID-19. Please check with your doctor to make sure.");
+                        return;
+                }
+                if (totalpoint >= 6 && counter == 11) {
+                        console.log("in")
+                        alert("Symptoms show that you may have COVID-19. Please make sure with your doctor or call 911 right away.");
+                }
+        }
+        console.log("out")
+}
